6aa2e648a0d1bad7ca56eec42a6def3a195efcca
Rainyjune-Kr <120240454+Rainyjune-Kr@users.noreply.github.com>
2023-02-28 22:03:14 +0900

    array, object state의 갱신

--- blog/src/App.js
+++ blog/src/App.js
@@ -13,10 +13,20 @@ function App() {
       <div className="black-nav">
         <h4>Rainyjune Blog</h4>
       </div>
+
+      <button onClick={ () => { 
+        let copy = [...articleTitle];
+        copy[0] = '여자 코트 추천';
+        setArticleTitle(copy);
+      } }>글 수정</button>
+
+      <button onClick={ () => {
+        let copy = [...articleTitle].sort();
+        setArticleTitle(copy);
+      }}>정렬</button>
+      
       <div className="list">
-        <h4 onClick={ () => { 
-          setArticleTitle(['여자 코트 추천', '강남 우동 맛집', '파이썬독학'])
-         } }>{ articleTitle[0] } <span onClick={ () => { setCntLike(cntLike + 1) } }>👍</span> { cntLike } </h4>
+        <h4>{ articleTitle[0] } <span onClick={ () => { setCntLike(cntLike + 1) } }>👍</span> { cntLike } </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
