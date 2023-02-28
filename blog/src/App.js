26fbb1c6b346765a9db5e4c49d02b8819488aade
Rainyjune-Kr <120240454+Rainyjune-Kr@users.noreply.github.com>
2023-02-28 21:41:42 +0900

    onClick, setState 실습

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,14 +6,17 @@ function App() {
   let [articleTitle, setArticleTitle] = useState(['남자 코트 추천', 
                                                   '강남 우동 맛집', 
                                                   '파이썬독학']);
-  
+  let [cntLike, setCntLike] = useState(0);
+
   return (
     <div className="App">
       <div className="black-nav">
         <h4>Rainyjune Blog</h4>
       </div>
       <div className="list">
-        <h4>{ articleTitle[0] }</h4>
+        <h4 onClick={ () => { 
+          setArticleTitle(['여자 코트 추천', '강남 우동 맛집', '파이썬독학'])
+         } }>{ articleTitle[0] } <span onClick={ () => { setCntLike(cntLike + 1) } }>👍</span> { cntLike } </h4>
         <p>2월 17일 발행</p>
       </div>
       <div className="list">
