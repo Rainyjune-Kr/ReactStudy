cb907e726ccc362c642cbc2f9b07dd188b68f11d
Rainyjune-Kr <120240454+Rainyjune-Kr@users.noreply.github.com>
2023-03-08 20:43:04 +0900

    react bootstrap 적용

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Rainyjune Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#hotdeal">핫딜</Nav.Link>
+            <Nav.Link href="#cart">장바구니</Nav.Link>
+            <Nav.Link href="#orders">구매내역</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
