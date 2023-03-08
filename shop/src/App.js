37dec1d11af5dcb4af7bad8b86118b429b96d063
Rainyjune-Kr <120240454+Rainyjune-Kr@users.noreply.github.com>
2023-03-08 21:21:12 +0900

    외부 리소스 참조

--- shop/src/App.js
+++ shop/src/App.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import imgSource from './bg.png';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
   return (
@@ -18,6 +21,28 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+
+      <div className='main-bg'style={ { backgroundImage: 'url('+ imgSource + ')' } }></div>
+
+      <Container>
+        <Row>
+          <Col>
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col>
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+          <Col>
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
